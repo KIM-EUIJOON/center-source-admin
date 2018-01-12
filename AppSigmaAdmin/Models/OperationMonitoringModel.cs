5ab8e3db92a20589336721e9303cd350ecf0e93f
Y.Wada <Y.Wada@cec-ltd.co.jp>
2018-01-12 18:59:40 +0900

    運用管理画面の暫定追加

---
+++ AppSigmaAdmin/Models/OperationMonitoringModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace AppSigmaAdmin.Models
+{
+    /// <summary>
+    /// 運用レポート出力　リクエストクラス
+    /// </summary>
+    public class OperationMonitoringModel
+    {
+        /// <summary>
+        /// 運用レポート出力年
+        /// </summary>
+        [Required]
+        [Display(Name = "年")]
+        [DataMember(Name = "year")]
+        public string Year { get; set; }
+
+        /// <summary>
+        /// 運用レポート出力月
+        /// </summary>
+        [Required]
+        [Display(Name = "月")]
+        [DataMember(Name = "month")]
+        public string Month { get; set; }
+    }
+}
