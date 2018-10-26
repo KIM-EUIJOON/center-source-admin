fe591241cab76f4d51f39e928dfcfab8c66167f4
m-watarai <m-watarai@cec-ltd.co.jp>
2018-10-26 18:35:40 +0900

    脆弱性診断対策として蓋締めしたコントローラーをコメントアウト

--- AppSigmaAdmin/Controllers/OperationMonitoringController.cs
+++ AppSigmaAdmin/Controllers/OperationMonitoringController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿/*using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
@@ -106,4 +106,4 @@ namespace AppSigmaAdmin.Controllers
             }
         }
     }
-}
+}*/
--- AppSigmaAdmin/Controllers/PaymentManageController.cs
+++ AppSigmaAdmin/Controllers/PaymentManageController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿/*using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -144,4 +144,4 @@ namespace AppSigmaAdmin.Controllers
             }
         }
     }
-}
+}*/
