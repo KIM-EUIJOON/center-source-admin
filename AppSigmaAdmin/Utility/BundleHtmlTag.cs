a7ff42d310bd55d8ed71a74cf2340a5b490f5f3a
Yasuhiro Furuyama <y_furuyama@cec-ltd.co.jp>
2019-06-07 14:21:46 +0900

    Bundleで仕様するカスタムFormatクラスを追加

---
+++ AppSigmaAdmin/Utility/BundleHtmlTag.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppSigmaAdmin.Library
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class BundleHtmlTag
+    {
+        /// <summary>
+        /// HTML Styleタグ
+        /// </summary>
+        public const string StyleTag = @"<link href=""/sigma/{0}"" rel=""stylesheet"" type=""text/css"" />";
+    }
+}
