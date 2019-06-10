cd2c197c6d28ff1eb2b63cc2e7f552236b6de122
Yasuhiro Furuyama <y_furuyama@cec-ltd.co.jp>
2019-06-10 11:58:45 +0900

    JavaScript及びStyleSheetのBundle対応

--- AppSigmaAdmin/App_Start/BundleConfig.cs
+++ AppSigmaAdmin/App_Start/BundleConfig.cs
@@ -9,41 +9,29 @@ namespace AppSigmaAdmin
         // バンドルの詳細については、https://go.microsoft.com/fwlink/?LinkId=301862 を参照してください
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/sigma/bundles/jquery").Include(
+            // JavaScript: JQuery関連
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
-            bundles.Add(new ScriptBundle("~/sigma/bundles/jquery-ui").Include(
+            bundles.Add(new ScriptBundle("~/bundles/jquery-ui").Include(
                         "~/Scripts/jquery-ui.js"));
-            bundles.Add(new ScriptBundle("~/sigma/bundles/jqueryval").Include(
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-
-
-            // 開発と学習には、Modernizr の開発バージョンを使用します。次に、実稼働の準備が
-            // 運用の準備が完了したら、https://modernizr.com のビルド ツールを使用し、必要なテストのみを選択します。
-            bundles.Add(new ScriptBundle("~/sigma/bundles/modernizr").Include(
+            // JavaScript: Modernizr関連
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/sigma/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+            // JavaScript: アプリケーション共通
+            bundles.Add(new ScriptBundle("~/bundles/jquery/common").Include(
+                        "~/Scripts/Pages/common.js"));
 
+            // StyleSheet: JQueryUI関連
+            bundles.Add(new StyleBundle("~/Content/jquery-ui/css").Include(
+                        "~/Content/jquery-ui.css"));
 
-
-            if (ApplicationConfig.DeployEnv == ApplicationConfig.ENV_DEBUG)
-            {
-                bundles.Add(new StyleBundle("~/sigma/Content/css").Include(
-                          "~/Content/bootstrap.css",
-                          "~/Content/jquery-ui.css",
-                          "~/Content/site.css"));
-            }
-            else
-            {
-                bundles.Add(new StyleBundle("~/sigma/Content/css").Include(
-                          "~/sigma/Content/bootstrap.css",
-                          "~/sigma/Content/jquery-ui.css",
-                          "~/sigma/Content/site.css"));
-            }
-
+            // StyleSheet: Application固有
+            bundles.Add(new StyleBundle("~/Content/app-site/css").Include(
+                        "~/Content/Site.css"));
         }
     }
 }
--- AppSigmaAdmin/Utility/BundleHtmlTag.cs
+++ AppSigmaAdmin/Utility/BundleHtmlTag.cs
@@ -13,6 +13,43 @@ namespace AppSigmaAdmin.Library
         /// <summary>
         /// HTML Styleタグ
         /// </summary>
-        public const string StyleTag = @"<link href=""/sigma/{0}"" rel=""stylesheet"" type=""text/css"" />";
+        public static string StyleTag
+        {
+            get
+            {
+                // 環境判別
+                if (ApplicationConfig.DeployEnv == ApplicationConfig.ENV_DEBUG)
+                {
+                    // デバッグ環境
+                    return @"<link href=""{0}"" rel=""stylesheet"" type=""text/css"" />";
+                }
+                else
+                {
+                    // 検証・号口環境
+                    return @"<link href=""/sigma/{0}"" rel=""stylesheet"" type=""text/css"" />";
+                }
+            }
+        }
+
+        /// <summary>
+        /// HTML Scriptタグ
+        /// </summary>
+        public static string ScriptTag
+        {
+            get
+            {
+                // 環境判別
+                if (ApplicationConfig.DeployEnv == ApplicationConfig.ENV_DEBUG)
+                {
+                    // デバッグ環境
+                    return @"<script src= ""{0}"" type=""text/javascript""></script>";
+                }
+                else
+                {
+                    // 検証・号口環境
+                    return @"<script src= ""/sigma/{0}"" type=""text/javascript""></script>";
+                }
+            }
+        }
     }
 }
